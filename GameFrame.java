ea91dfa5166961b8d55c5e9c1bb0679b70ecff58
Protein-BOR <127687812+Protein-BOR@users.noreply.github.com>
2024-10-17 14:51:36 -0500

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+    GameFrame() {
+        this.add(new GamePanel());
+        this.setTitle("Snake Game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false); // Make sure the window isn't resizable
+        this.pack(); // Sizes the frame so all contents are at preferred sizes
+        this.setVisible(true); // Make the frame visible
+        this.setLocationRelativeTo(null); // Center the window on the screen
+    }
+
+    public static void main(String[] args) {
+        new GameFrame(); // Create and display the game window
+    }
+}
